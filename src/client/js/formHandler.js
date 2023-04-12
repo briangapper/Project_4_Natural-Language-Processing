fc9bdec66c6fab33126086f551678dc495f4f4a1
briangapper <46603246+briangapper@users.noreply.github.com>
2023-04-12 16:07:09 +0200

    Init Files

---
+++ src/client/js/formHandler.js
@@ -0,0 +1,18 @@
+function handleSubmit(event) {
+    event.preventDefault()
+
+    // check what text was put into the form field
+    let formText = document.getElementsById('name').value
+    checkForName(formText)
+
+    console.log('::: Form Submitted :::')
+    fetch('https://localhost:8080/test')
+    .then(res => res.json())
+    .then(function(res){
+        document.getElementsById('results').innerHTML = res.message
+    })
+}
+
+export {
+    handleSubmit
+}
