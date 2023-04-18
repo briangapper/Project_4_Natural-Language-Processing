cf6c08b6e9538ee45f5b8a768e855f7c8573c5bb
briangapper <46603246+briangapper@users.noreply.github.com>
2023-04-18 15:55:01 +0200

    Version 1.0

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,14 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-import './styles/resets.scss'
-import './styles/base.scss'
-import './styles/form.scss'
-import './styles/footer.scss'
-import './styles/header.scss'
+// import './styles/resets.scss'
+// import './styles/base.scss'
+// import './styles/form.scss'
+// import './styles/footer.scss'
+// import './styles/header.scss'
 
 alert('I EXIST!')
+alert('I EXIST2!')
 
 export {
     checkForName,
