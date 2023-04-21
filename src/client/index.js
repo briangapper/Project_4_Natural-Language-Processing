11a533e288a7e7aa4798f5c890c6656f40b2231c
briangapper <46603246+briangapper@users.noreply.github.com>
2023-04-21 20:58:01 +0200

    Set up server and API

--- src/client/index.js
+++ src/client/index.js
@@ -7,6 +7,8 @@ import './styles/form.scss'
 import './styles/footer.scss'
 import './styles/header.scss'
 
+
+
 export {
     checkForName,
     handleSubmit
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,28 @@
-var path = require('path')
-const express = require('express')
-const cors = require('cors')
-const mockAPIResponse = require('./mockAPI.js')
+// ****************************************
+// ----------------------------------------
+// 1.) REQUIRES
+// ----------------------------------------
+// ****************************************
+var path = require('path');
 
+// express: web application framework for Node.js that provides a set of features and tools to build web applications and APIs
+const express = require('express');
+// cors: used to protect web servers from unauthorized access, malicious attacks, and data theft
+const cors = require('cors');
+// dotenv: used to load environment variables from a .env file into the process.env object in Node.js
+const dotenv = require('dotenv');
+dotenv.config();
+// axios: used to make HTTP requests from the Node.js application to the MeaningCloud API
+const axios = require('axios');
+// local import
+const mockAPIResponse = require('./mockAPI.js');
+
+// ****************************************
+// ----------------------------------------
+// 2.) SERVER CONFIG
+// ----------------------------------------
+// ****************************************
 const app = express()
-
-// Add CORS middleware
 app.use(cors())
 
 app.use(express.static('dist/prod'))
@@ -16,7 +33,6 @@ app.get('/', function(req, res){
     // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
-// designates what port the app will listen to for incoming requests
 app.listen(9000, function(){
     console.log('App listening on port 9000!')
 })
@@ -24,3 +40,23 @@ app.listen(9000, function(){
 app.get('/test', function(req, res){
     res.send(mockAPIResponse)
 })
+
+// ****************************************
+// ----------------------------------------
+// 3.) API CONFIG
+// ----------------------------------------
+// ****************************************
+async function getSentiment(text){
+    const url = `https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&lang=en&txt=${text}`
+    const response = await axios.get(url)
+    return response.data
+}
+
+getSentiment('This is a positive sentence.')
+  .then(data => {
+    console.log('Message: ')
+    console.log(data)
+  })
+  .catch(error => {
+    console.log(error)
+  })
